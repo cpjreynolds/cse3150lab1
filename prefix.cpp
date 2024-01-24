50fee9b740e5b26088e6ce4c9c5fac3f3623f0c8
Cole Reynolds <cpjreynolds@gmail.com>
2024-01-24 13:35:20 -0500

    test prefix fns

--- prefix.cpp
+++ prefix.cpp
@@ -1,35 +1,104 @@
-#include <cstddef>
-#include <iostream>
-#include <array>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
-using std::array;
-using std::size_t;
-
-template<size_t N>
-int prefix_sum(const array<int, N>& lst)
+/*
+ * Takes any integer iterable and tests its prefix sums for non-negativity
+ */
+template<class I>
+bool non_neg_prefix_sum(const I& lst)
 {
     int sum = 0;
     for (const int& i : lst) {
         sum += i;
+        if (sum < 0) {
+            return false;
+        }
     }
-    return sum;
+    return true;
 }
 
-template<size_t N>
-bool non_neg_prefix_sum(const array<int, N>& lst)
+/*
+ * Takes any integer iterable and tests its prefix sums for non-positivity
+ */
+template<class I>
+bool non_pos_prefix_sum(const I& lst)
 {
-    return prefix_sum(lst) > 0;
+    int sum = 0;
+    for (const int& i : lst) {
+        sum += i;
+        if (sum > 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-template<size_t N>
-bool non_pos_prefix_sum(const array<int, N>& lst)
+TEST_CASE("non_neg_prefix_sum")
 {
-    return !non_neg_prefix_sum(lst);
+    SUBCASE("correctly returns true")
+    {
+        const auto data = {1, -1, 1, -1};
+        CHECK(non_neg_prefix_sum(data));
+    }
+
+    SUBCASE("correctly returns false")
+    {
+        const auto data = {1, -1, -1, 1};
+        CHECK_FALSE(non_neg_prefix_sum(data));
+    }
+
+    SUBCASE("handles zero-length input")
+    {
+        const int data[] = {};
+        CHECK(non_neg_prefix_sum(data));
+    }
+
+    SUBCASE("handles long input")
+    {
+        // create a long sequence of {1,-1,1,...}
+        std::vector<int> data;
+        const int n[2]{1, -1};
+        for (size_t i = 0; i < 1 << 20; ++i) {
+            data.push_back(n[i % 2]);
+        }
+        CHECK(non_neg_prefix_sum(data));
+        // check for false as well
+        std::swap(data[1 << 10], data[(1 << 10) + 1]);
+        CHECK_FALSE(non_neg_prefix_sum(data));
+    }
 }
 
-int main()
+TEST_CASE("non_pos_prefix_sum")
 {
-    array n{1, 2, 3};
-    int x = prefix_sum(n);
-    std::cout << x << std::endl;
+    SUBCASE("correctly returns true")
+    {
+        const auto data = {-1, 1, -1, 1};
+        CHECK(non_pos_prefix_sum(data));
+    }
+
+    SUBCASE("correctly returns false")
+    {
+        const auto data = {1, -1, -1, 1};
+        CHECK_FALSE(non_pos_prefix_sum(data));
+    }
+
+    SUBCASE("handles zero-length input")
+    {
+        const int data[] = {};
+        CHECK(non_pos_prefix_sum(data));
+    }
+
+    SUBCASE("handles long input")
+    {
+        // create a long sequence of {-1,1,-1,...}
+        std::vector<int> data;
+        const int n[2]{-1, 1};
+        for (size_t i = 0; i < 1 << 20; ++i) {
+            data.push_back(n[i % 2]);
+        }
+        CHECK(non_pos_prefix_sum(data));
+        // check for false as well
+        std::swap(data[1 << 10], data[(1 << 10) + 1]);
+        CHECK_FALSE(non_pos_prefix_sum(data));
+    }
 }
